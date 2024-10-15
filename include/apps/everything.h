9ae3b1cde393ac53a0d570c7fdfd2d169c739ffc
aceinetx <mouselin82@gmail.com>
2024-10-15 14:37:14 +0300

    huge update

---
+++ include/apps/everything.h
@@ -0,0 +1,7 @@
+#if !defined(EVERYTHING_H)
+#define EVERYTHING_H
+
+#include <stdbool.h>
+extern bool active;
+
+#endif // EVERYTHING_H
---
+++ src/virtual_desktop.c
@@ -0,0 +1,6 @@
+#include <virtual_desktop.h>
+
+char desktop_id = 1;
+
+char* get_desktop_id_ptr() { return &desktop_id; };
+char get_max_desktops() { return 4; }
