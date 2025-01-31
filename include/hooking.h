807a7f9b8526983d8fcfaf0cc7124dbc79924dae
aceinetx <mouselin82@gmail.com>
2025-01-31 16:06:29 +0300

    Arch update #4

--- include/hooking.h
+++ include/hooking.h
@@ -2,15 +2,18 @@
 #define HOOKING_H
 
 #if defined(ENDUI_X86)
-
 #include <libmem/libmem.h>
+#define addr_t lm_address_t
 #define endui_hook(from, to, trampoline) LM_HookCode((lm_address_t)from, (lm_address_t)to, (lm_address_t *)trampoline)
 #endif
 
 #if defined(ENDUI_ARM)
-#error "Hooking not implemented for ARM"
+#include <dobby.h>
+#define addr_t void*
+#define endui_hook(from, to, trampoline) DobbyHook((void*)from, (dobby_dummy_func_t)to, (dobby_dummy_func_t*)trampoline)
 #endif
 
+// Check if everything is defined
 #ifndef endui_hook
 #error "endui_hook not defined, probably unknown architecture"
 #endif
