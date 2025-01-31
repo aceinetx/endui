211a8054b6a0f7dd17e408b2e437e30923485c0b
aceinetx <mouselin82@gmail.com>
2025-01-31 13:31:14 +0300

    Arch update #1

--- include/hooking.h
+++ include/hooking.h
@@ -1,7 +1,20 @@
 #if !defined(HOOKING_H)
 #define HOOKING_H
 
+#if defined(ENDUI_X86)
+
 #include <libmem/libmem.h>
+
 #define endui_hook(from, to, trampoline) LM_HookCode((lm_address_t)from, (lm_address_t)to, (lm_address_t *)trampoline)
+
+#elif defined(ENDUI_ARM)
+
+#error "Hooking not implemented for ARM"
+
+#else
+
+#error "Unknown architecture"
+
+#endif
 
 #endif // HOOKING_H
